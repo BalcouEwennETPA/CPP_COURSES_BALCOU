cd92f40cf0d7d15d1737661d679b8274089b8fa2
BalcouEwennETPA <ewennbalcou10@gmail.com>
2021-03-10 14:33:15 +0100

    start

---
+++ course.h
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+class Course{
+    private :
+        std::string _Pseudo;
+        std::string _nameCourse;
+        int _score;
+        int _Bestscorerace;
+    public :
+        std::string getPseudo;
+        std::string get
+    }
