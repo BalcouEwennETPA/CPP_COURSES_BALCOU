11884cf244285a3c9a6bf4094201550d9488489d
BalcouEwennETPA <ewennbalcou10@gmail.com>
2021-03-10 15:21:15 +0100

    name

--- CPP_COURSES_BALCOU.cpp
+++ CPP_COURSES_BALCOU.cpp
@@ -3,3 +3,13 @@
 #include "course.cpp"
 
 using namespace std;
+
+int main(){
+    Course race1 ("Mario","Route arc-en-ciel","50");
+    Course race2 ("Waluigi","Temple Thwomp","48");
+    Course race3 ("Bowser","Big Blue","35");
+    Course race4 ("Peach","Koopapolis","67")
+    Course race5 ("Yoshi","Parc Baby","37");
+
+    
+}
